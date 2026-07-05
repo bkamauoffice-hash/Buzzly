727402c4746307f90386c910e90c39412a44664f
bkamauoffice-hash <bkamau.office@gmail.com>
2026-07-05 19:36:02 +0300

    Create firebase.js

---
+++ js/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAW-eIuB6mhVGW1k_URLJCf4hJfDGey7H8",
+  authDomain: "buzzly-56f75.firebaseapp.com",
+  projectId: "buzzly-56f75",
+  storageBucket: "buzzly-56f75.firebasestorage.app",
+  messagingSenderId: "526109842842",
+  appId: "1:526109842842:web:52583fc8dc905cf2baee23",
+  measurementId: "G-QGPLKG9PX1"
+};
